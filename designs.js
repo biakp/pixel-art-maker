bc54077e71ecdd812c5479c42c4ffe748230581c
Biakp <42621272+biakp@users.noreply.github.com>
2018-11-23 19:53:11 -0200

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,41 @@
+    //Variables
+    // Select color input
+    const colorInput = $('#colorPicker');
+    // Select size input
+    const inputHeight = $('#inputHeight');
+    const inputWidth = $('#inputWidth');
+    const canvas = $('#pixelCanvas');
+    // Select Canvas
+    //Form
+    const sizePicker = $('.sizePicker');
+    //Functions 
+    function makeGrid() {
+        let height = inputHeight.val();
+        let width = inputWidth.val();
+        canvas.innerHTML = '';
+        for (let x = 0; x < height; x++) {
+            let tr = document.createElement('tr');
+            for (let z = 0; z < width; z++) {
+                let td = document.createElement('td');
+                td.append(document.createTextNode(''));
+                tr.append(td);
+            }
+            canvas.empty().append(tr);
+        }
+    };
+    //Change pixel color
+    function pixelClick(event) {
+        let color = colorInput;
+        $('body').on('click', 'td', function () {
+            $(this).css('background-color', color.val());
+            console.log("oohaiio");
+        });
+    }
+
+
+    // When size is submitted by the user, call makeGrid()
+    document.querySelector('form').addEventListener('submit', function (event) {
+        makeGrid();
+        pixelClick();
+        event.preventDefault();
+    });
